e3a3cf49f6e866f17e1e8fc2e85598af15974bb3
tomshy94 <tomnyongesa@gmail.com>
2019-01-12 12:12:36 +0300

    fixed bar chart

--- office_admin/templates/home.tpl.php
+++ office_admin/templates/home.tpl.php
@@ -13,17 +13,17 @@ if($action=='birthday_students')
 {?>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
-    	<td height="3" colspan="3"></td>
+        <td height="3" colspan="3"></td>
     </tr>
     
     <tr>
-    	<td height="25" colspan="3" class="bgcolor_02"><strong>&nbsp;&nbsp;<span id="internal-source-marker_0.052443267584382114">This Month Student's Birthday List</span></strong></td>
+        <td height="25" colspan="3" class="bgcolor_02"><strong>&nbsp;&nbsp;<span id="internal-source-marker_0.052443267584382114">This Month Student's Birthday List</span></strong></td>
     </tr>
     
-	<tr>
+    <tr>
         <td width="1" class="bgcolor_02"></td>
         <td align="left" valign="top"><br />
-			<table width="100%" border="0">
+            <table width="100%" border="0">
                 <tr height="30" class="bgcolor_02">
                     <td width="7%" align="center" valign="middle">&nbsp;S No</td>
                     <td width="26%" align="left" valign="middle">&nbsp;Student Name</td>
@@ -32,7 +32,7 @@ if($action=='birthday_students')
                     <td width="9%" align="center" valign="middle">&nbsp;Reg ID </td>
                     <td width="15%" align="center" valign="middle">&nbsp;DOB</td>
                 </tr>
-				<?php 
+                <?php 
                 
                 //$students_det = $db->getrows($sql_todaybirth);
                 
@@ -41,7 +41,7 @@ if($action=='birthday_students')
                 $i=0; 
                 foreach($students_det as $each)
                 {
-                //	echo $each['es_preadmissionid'];
+                //  echo $each['es_preadmissionid'];
                 
                 list($year, $month) = explode('-', date('Y-n'));
                 $date = getdate();
@@ -72,19 +72,19 @@ if($action=='birthday_students')
 
    if(count($students_det)>=1)
    {
-		$i=0;
-		foreach($students_det as $each)
-		{
-    		list($year, $month) = explode('-', date('Y-n'));
-			$date = getdate();
-			$year = $date['year'];
-			$month = $date['mon'];
+        $i=0;
+        foreach($students_det as $each)
+        {
+            list($year, $month) = explode('-', date('Y-n'));
+            $date = getdate();
+            $year = $date['year'];
+            $month = $date['mon'];
  
-			//echo substr($each['pre_dateofbirth'],5,2);
+            //echo substr($each['pre_dateofbirth'],5,2);
 
-			if(substr($each['pre_dateofbirth'],5,2)==$month)
-			{
-				$i++;
+            if(substr($each['pre_dateofbirth'],5,2)==$month)
+            {
+                $i++;
 ?>
                 <tr>
                     <td align="center" valign="middle">&nbsp;<?php echo $i;?></td>
@@ -92,33 +92,33 @@ if($action=='birthday_students')
                     <td align="left" valign="middle">&nbsp;<?php echo classname($each['pre_class']);?></td>
                     <td align="left" valign="middle">&nbsp;<?php echo ucwords($each['pre_fathername']);?></td>
                     <td align="center" valign="middle">&nbsp;
-	 				<?php echo ucwords($each['es_preadmissionid']);
-					/*  $section_det1 = "SELECT * FROM es_sections_student SS , es_sections S WHERE SS.student_id='".$each['es_preadmissionid']."' AND SS.course_id='".$each['pre_class']."' AND SS.section_id=S.section_id ";
-					//   $section_det=$db->getrows(section_det1);
-					$res=mysql_query($section_det1);
-					$row=mysql_fetch_array($res);
-					echo $row['section_name'];*/
-					/*?>	 
-					if($section_det['section_name']!=""){
-					echo ucwords($section_det['section_name']);}else{echo "---";}?></td>
-					<td>&nbsp;<?php  if($section_det['roll_no']!=""){
-					echo $section_det['roll_no'];}else{echo "---";}?><?php */?></td>
-					<td align="center" valign="middle">&nbsp;<?php echo displaydate($each['pre_dateofbirth']);?></td>
-				</tr>
-<?php		}// End of if(substr($each['pre_dateofbirth'],5,2)==$month)
-		}// End of foreach($students_det as $each)
-	}//End of if(count($students_det)>=1)
-	else
-	{
+                    <?php echo ucwords($each['es_preadmissionid']);
+                    /*  $section_det1 = "SELECT * FROM es_sections_student SS , es_sections S WHERE SS.student_id='".$each['es_preadmissionid']."' AND SS.course_id='".$each['pre_class']."' AND SS.section_id=S.section_id ";
+                    //   $section_det=$db->getrows(section_det1);
+                    $res=mysql_query($section_det1);
+                    $row=mysql_fetch_array($res);
+                    echo $row['section_name'];*/
+                    /*?>     
+                    if($section_det['section_name']!=""){
+                    echo ucwords($section_det['section_name']);}else{echo "---";}?></td>
+                    <td>&nbsp;<?php  if($section_det['roll_no']!=""){
+                    echo $section_det['roll_no'];}else{echo "---";}?><?php */?></td>
+                    <td align="center" valign="middle">&nbsp;<?php echo displaydate($each['pre_dateofbirth']);?></td>
+                </tr>
+<?php       }// End of if(substr($each['pre_dateofbirth'],5,2)==$month)
+        }// End of foreach($students_det as $each)
+    }//End of if(count($students_det)>=1)
+    else
+    {
 ?>
         <tr>
-        	<td colspan="6" align="center">No Students Found</td>
+            <td colspan="6" align="center">No Students Found</td>
         </tr>
 <?php
-	}
+    }
 ?>
 </table>
-			  </td>
+              </td>
                 <td width="1" class="bgcolor_02"></td>
               </tr>
                 <td height="1" colspan="3" class="bgcolor_02"></td>
@@ -165,26 +165,34 @@ else
         </tr>
     </table>
     <div>
+        <style type="text/css">
+            svg{
+                margin-left: 30px;
+            }
+            .bar:hover{
+                background-color: brown;
 
-        <svg style="margin-left=35px"></svg>
+            }
+        </style>
+        <svg></svg>
     </div>
 <?php
 }?>
 </body>
 <script type="text/javascript">
     var height=350;
-    var width=800;
-    var svg=d3.select("svg").attr("height",height).attr("width",width);
+    var width=630;
+    var svg=d3.select("svg").attr("height",height+50).attr("width",width);
     
     d3.select("h2").style("color","darkblue");
-    var kcse=[4.2,6.0,5.6,6.4,5.3];
+    var kcse=[4.2,6.0,5.6,3.8,5.3];
     var y_scale=d3.scaleLinear()
-            .domain([0,d3.max(kcse)])
-            .range([300,0]);
+            .domain([0,7])
+            .range([350,0]);
     var years=[2013,2014,2015,2016,2017];
     var x_scale=d3.scaleLinear()
-            .domain(14,17)
-            .range(0,700);
+            .domain([13,17])
+            .range([0,498]);
     var x_axis=d3.axisBottom()
             .scale(x_scale);
     var y_axis=d3.axisLeft()
@@ -200,7 +208,7 @@ else
             .attr("width",bar_width-1)
             .attr("height",function(d){return height-y_scale(d);})
             .attr("transform", function(d, i) {
-            var translate=[35+bar_width*i,0];
+            var translate=[30+bar_width*i,0];
             return "translate("+translate+")";
           }).style("fill","#27d4a8");
     svg.selectAll("text")
@@ -208,14 +216,14 @@ else
         .enter()
         .append("text")
         .text(function(d){return d;})
-        .attr("y",function(d,i){return height-y_scale(d)-1;})
-        .attr("x",function(d,i){return bar_width*i+60;})
+        .attr("y",function(d,i){return y_scale(d)-1;})//y position of bar labels
+        .attr("x",function(d,i){return bar_width*i+70;})//x position of bar labels
         .style("fill","#999999");
     svg.append("g")
-        .attr("transform","translate(35,45)")
+        .attr("transform","translate(30,0)")
         .call(y_axis);
     svg.append("g")
-        .attr("transform","translate(35,20)")
+        .attr("transform","translate(30,350)")
         .call(x_axis);
     </script>
 </html>
